dae8dc65ea43591910bb5160f5360885ace85328
Yume Rokku <163641570+tuanloc782004@users.noreply.github.com>
2025-01-10 23:17:36 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let words = document.querySelectorAll(".word")
+words.forEach((word)=>{
+    let letters = word.textContent.split("")
+    word.textContent=""
+    letters.forEach((letter)=>{
+        let span = document.createElement("span")
+        span.textContent = letter
+        span.className = "letter"
+        word.append(span)
+    });
+});
+
+let currentWordIndex = 0
+let maxWordIndex = words.length - 1
