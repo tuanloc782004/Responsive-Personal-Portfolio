4e094c0ec99af3d0b3500ea960c95869c8b5809e
Yume Rokku <163641570+tuanloc782004@users.noreply.github.com>
2025-01-15 19:33:10 +0700

    4th update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let words = document.querySelectorAll(".word")
-words.forEach((word)=>{
+words.forEach((word) => {
     let letters = word.textContent.split("")
-    word.textContent=""
-    letters.forEach((letter)=>{
+    word.textContent = ""
+    letters.forEach((letter) => {
         let span = document.createElement("span")
         span.textContent = letter
         span.className = "letter"
@@ -14,19 +14,19 @@ let currentWordIndex = 0
 let maxWordIndex = words.length - 1
 words[currentWordIndex].style.opacity = "1"
 
-let changeText = ()=>{
+let changeText = () => {
     let currentWord = words[currentWordIndex]
     let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1]
 
-    Array.from(currentWord.children).forEach((letter, i)=>{
-        setTimeout(()=>{
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
             letter.className = "letter out";
         }, i * 80);
     });
     nextWord.style.opacity = "1"
-    Array.from(nextWord.children).forEach((letter, i)=>{
+    Array.from(nextWord.children).forEach((letter, i) => {
         letter.className = "letter behind"
-        setTimeout(()=>{
+        setTimeout(() => {
             letter.className = "letter in"
         }, 340 + i * 80);
     });
@@ -35,3 +35,24 @@ let changeText = ()=>{
 
 changeText();
 setInterval(changeText, 3000)
+
+// circle skill ///
+
+const circles = document.querySelectorAll('.circle')
+circles.forEach(elem => {
+    var dots = elem.getAttribute("data-dots")
+    var marked = elem.getAttribute("data-percent")
+    var percent = Math.floor(dots * marked / 100)
+    var points = "";
+    var rotate = 360 / dots
+
+    for (let i = 0; i < dots; i++) {
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`
+    }
+    elem.innerHTML = points
+
+    const pointsMarked = elem.querySelectorAll('.points')
+    for (let i = 0; i < percent; i++) {
+        pointsMarked[i].classList.add('marked')
+    }
+})
